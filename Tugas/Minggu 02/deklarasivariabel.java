d8a2427649fc63ac7f522e47c0f518d3a28b6ab6
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2021-03-16 21:21:02 +0700

    Create deklarasivariabel.java

---
+++ Tugas/Minggu 02/deklarasivariabel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_c;
+
+/**
+ *
+ * @author Acer
+ */
+public class deklarasivariabel {
+    
+    boolean gameOver = false; // Pendeklarasian variabel benar
+    int student=50,classes=3; // Pendeklarasian variabel benar
+    double sales_tax; // Pendeklarasian variabel benar
+    short number1; // Pendeklarasian variabel benar
+    
+    int 2beOrNot2be; // Penamaan variabel tidak boleh diawali dengan angka
+    float price index; // Penamaan variabel tidak boleh menggunakan spasi
+    double lastYear'sPrice;
+    // Penamaan varibale tidak boleh menggunakan simbol kecuwali untuk underscore (_)
+    long class; // Penamaan variabel tidak boleh menggunakan keywoard yang sudah ada didalam program java
+}
