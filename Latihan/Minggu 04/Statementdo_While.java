b672bd103075866bc8b21548efe5ead4dcb402ff
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2021-03-30 17:33:27 +0700

    Create Statementdo_While.java

---
+++ Latihan/Minggu 04/Statementdo_While.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanB_Statementdo_While;
+
+/**
+ *
+ * @author Acer
+ */
+public class Statementdo_While {
+    public static void main(String[] args) {
+        // membuat variabel
+        int i = 0;
+        
+        do {
+            System.out.println("Perulangan Ke-" + i);
+            i ++;
+        } while ( i <= 10);
+    }
+}
