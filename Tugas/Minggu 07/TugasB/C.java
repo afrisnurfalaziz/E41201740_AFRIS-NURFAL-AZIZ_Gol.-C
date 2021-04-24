ee612d5c14ea0edc51c951e1713d70ee4accdd6f
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2021-04-25 03:50:35 +0700

    Create C.java

---
+++ Tugas/Minggu 07/TugasB/C.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasB;
+
+/**
+ *
+ * @author Acer
+ */
+public class C extends A {
+    @Override
+    void callthis() {
+        System.out.println("Iside Class C's Method!");
+    }
+}
