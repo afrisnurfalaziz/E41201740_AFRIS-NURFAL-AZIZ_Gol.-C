71cba4ee0e43b825761665e3eaa808cbdb1f256b
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2021-04-25 03:46:01 +0700

    Create Pelajar.java

---
+++ Latihan/Minggu 07/Orang/Pelajar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Orang;
+
+/**
+ *
+ * @author Acer
+ */
+public class Pelajar extends Orang {
+    private String nim;
+    private String asalSekolah;
+    private double nilai;
+    
+    public Pelajar (String nama, double tinggi, double berat, String nim, 
+            String sekolah, double nilai) {
+        super (nama, tinggi, berat);
+        this.nim = nim;
+        asalSekolah = sekolah;
+        this.nilai = nilai;
+    }
+    @Override
+    public String toString() {
+        return (super.toString() + "\nNIM :" + nim + "\nSekolah :" + asalSekolah
+                + "\nNilai : " + nilai);
+    }
+}
