60a8d61e412b7bab00121d985cf2438e0ed8eafb
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2021-04-25 03:45:35 +0700

    Create B.java

---
+++ Latihan/Minggu 07/Konstruktor/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Konstruktor;
+
+/**
+ *
+ * @author Acer
+ */
+public class B extends A {
+    B() {
+        super ("test ");
+        System.out.println("Konstrukso class B dieksekusi ...");
+    }
+}
